fb512451179a2cb37847c1516717ed0913746e88
Ingrid <silva_ingrid@outlook.com>
2021-08-25 16:35:25 -0300

    Correção do header e Pokecard

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,34 +1,12 @@
-import React from "react";
-// import styled from "styled-components";
-// import Button from "@material-ui/core/Button";
-// import { ThemeProvider } from "@material-ui/styles";
-// import { theme } from "../../Theme";
+import Header from "../../components/Header";
+import PokeCard from "../../components/PokeCard";
 
 const HomePage = () => {
-  // const Header = styled.div`
-  //   display: flex;
-  //   justify-content: space-between;
-  //   align-items: center;
-  //   padding: 0 3px;
-  //   border: 2px solid black;
-  //   background-color: red;
-  // `;
+  
 
   return (
-    // É NECESSÁRIO EXTRAIR O COMPONENTE PARA UM ARQUIVO .JS SEPARADO PARA SER REAPROVEITADO NO SITE, ESSE CÓDIGO DE HEADER SÓ ESTÁ FUNCIONANDO NA PÁGINA HOME, NAS OUTRAS ELE NÃO VAI APARECER.
-
-    // <Header>
-    //   <ThemeProvider theme={theme}>
-    //     <Button variant="contained" color="primary">
-    //       Voltar
-    //     </Button>
-    //     <h1> Pokédex</h1>
-    //     <Button variant="contained" color="primary">
-    //       Adicionar/Remover da Pokédex
-    //     </Button>
-    //   </ThemeProvider>
-    // </Header>
-    <div>HomePage</div>
+<div>Homepage</div>
+   
   );
 };
 
